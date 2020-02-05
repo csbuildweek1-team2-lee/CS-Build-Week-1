1336de775c71e4393d34c0284a48ec521c52322f
Reed James <reedtjames1234@gmail.com>
2020-02-05 17:12:32 -0600

    It looks like its working with rooms and stuff and moving

--- util/create_rooms.py
+++ util/create_rooms.py
@@ -1,3 +1,8 @@
+# from django.contrib.auth.models import User
+from adventure.models import Player, Room
+# from util.rooms import room_names, room_descriptions
+# from util.create_rooms import World
+
 room_names = [
     "The Creepy Mine", "The Fort of the Fury", "The golden Turret of the Destruction", "The Lab of the Blessing",
     "The Apothecary of the Renewal", "The Elemental Treasury of Renewal", "The Fortress of the Reawakened", "The Green Garden of Chaos",
@@ -125,9 +130,114 @@ room_descriptions = [
 ]
 
 
+class World:
+  def __init__(self):
+    self.grid = None
+    self.width = 0
+    self.height = 0
 
-# print(len(room_descriptions))
-# print(len(room_names))
+  def generate_rooms(self, size_x, size_y, num_rooms):
+    '''
+    Fill up the grid, bottom to top, in a zig-zag pattern
+    '''
 
+    # Initialize the grid
+    Room.objects.all().delete()
+    self.grid = [None] * size_y
+    self.width = size_x
+    self.height = size_y
+    # for i in range(len(self.grid)):
+    for i in range(0, len(self.grid)):
+      self.grid[i] = [None] * size_x
+      # Start from lower-left corner (0,0)
+    x = -1  # (this will become 0 on the first step)
+    y = 0
+    room_count = 0
+    # Start generating rooms to the east
+    direction = 1  # 1: east, -1: west
+    # While there are rooms to be created...
+    previous_room = None
+    while room_count < num_rooms:
+      # Calculate the direction of the room to be created
+      if direction > 0 and x < size_x - 1:
+        room_direction = "e"
+        x += 1
+      elif direction < 0 and x > 0:
+        room_direction = "w"
+        x -= 1
+      else:
+        # If we hit a wall, turn north and reverse direction
+        room_direction = "n"
+        y += 1
+        direction *= -1
+      # Create a room in the given direction
+      room = Room(room_count, room_names[room_count], room_descriptions[room_count],  x, y)
+      # Save the room in the World grid
+      self.grid[y][x] = room
+      # Connect the new room to the previous room
+      room.save()
+      if previous_room is not None:
+        # print(room.id, previous_room.id)
+        previous_room.connectRooms(room, room_direction)
 
+      # Update iteration variables
+      previous_room = room
+      room_count += 1
+      # print(f'room: {room}')
+  def print_rooms(self):
+    '''
+    Print the rooms in room_grid in ascii characters.
+    '''
+    # Add top border
+    str = "# " * ((3 + self.width * 5) // 2) + "\n"
+    # The console prints top to bottom but our array is arranged
+    # bottom to top.
+  # We reverse it so it draws in the right direction.
+    reverse_grid = list(self.grid) # make a copy of the list
+    reverse_grid.reverse()
+    for row in reverse_grid:
+      # PRINT NORTH CONNECTION ROW
+      str += "#"
+      for room in row:
+        if room is not None and room.n_to != 0:
+          str += "  |  "
+        else:
+          str += "     "
+        str += "#\n"
+        # PRINT ROOM ROW
+        str += "#"
+        for room in row:
+            if room is not None and room.w_to != 0:
+                str += "-"
+            else:
+                str += " "
+            if room is not None:
+                str += f"{room.id}".zfill(3)
+            else:
+                str += "   "
+            if room is not None and room.e_to != 0:
+                str += "-"
+            else:
+                str += " "
+        str += "#\n"
+        # PRINT SOUTH CONNECTION ROW
+        str += "#"
+        for room in row:
+            if room is not None and room.s_to != 0:
+                str += "  |  "
+            else:
+                str += "     "
+        str += "#\n"
+        # Add bottom border
+        str += "# " * ((3 + self.width * 5) // 2) + "\n"
+        # Print string
+        print(str)
 
+# w = World()
+# num_rooms = 100
+# width = 10
+# height = 10
+# w.generate_rooms(width, height, num_rooms)
+# w.generate_rooms(10, 10, 100)
+
+# print(f"\n\nWorld\n  height: {height}\n  width: {width},\n  num_rooms: {num_rooms}\n")
